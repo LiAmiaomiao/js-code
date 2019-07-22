a703fb88f9dda0851e29d91e4d0437ef92517e9c
LiAmiao <L_ruli@163.com>
2019-07-22 22:36:42 +0800

    js code

---
+++ react-achieve/react.js
@@ -0,0 +1,11 @@
+const randomLikes = ()=> Math.ceil(Math.random()*100);
+const stories = [
+    {name: 'React', url: "https://reactjs.org", likes: randomLikes()},
+    {name: 'Node', url: 'https://nodejs.org/en/',likes:randomLikes()},
+    {name:'Webpack',url:'https://webpack.js.org/',likes:randomLikes()}
+];
+const ItemRander = props=>{
+
+}
+
+
--- sort/quike-sort.js
+++ sort/quike-sort.js
@@ -1,6 +1,18 @@
 //快速排序
 function quikeSort(arr){
     if(arr.length <= 1){
-
+        return arr;
     }
+    var leftArr = [];
+    var rightArr = [];
+    var q = arr[0];
+    for(var i=0,len=arr.length;i<len;i++){
+        if(arr[i] > q){
+            rightArr.push(arr[i]);
+        }else{
+            leftArr.push(arr[i]);
+        }
+    }
+    return [].concat(quikeSort(leftArr),[q], quikeSort(rightArr));
 }
+moudle.exports = quikeSort;
