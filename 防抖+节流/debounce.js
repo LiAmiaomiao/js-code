21f36c639334725b11b5c61aae9a1a9ce488cd08
LiAmiao <L_ruli@163.com>
2019-09-15 18:52:12 +0800

    js code

--- 防抖+节流/debounce.js
+++ 防抖+节流/debounce.js
@@ -1,4 +1,5 @@
 //防抖
+//在任务频繁触发的时候，若在一段时间内再触发这个函数，则函数重新计时
 const debounce = (fn, delay) => {
     let timer = null;
     return (...args) => {
--- 防抖+节流/throttle.js
+++ 防抖+节流/throttle.js
@@ -1,3 +1,14 @@
+//节流
+//指定时间间隔内执行一次任务
+//在任务频繁触发的时候，在时间间隔内只触发一次
 const throttle = (fn, delay=500) => {
-
+    let flag = true;
+    return (...args)=>{
+        if(!flag) return;
+        flag=false;
+        setTimeout((...args)=>{
+            fn.apply(this,args);
+            flag=true;
+        },delay)
+    }
 }
