4a97af23c278224013a834e11fb3224fbe3c85b8
LiAmiao <L_ruli@163.com>
2019-11-25 21:14:01 +0800

    js code

--- deep clone/deepClone简单版.js
+++ deep clone/deepClone简单版.js
@@ -1,14 +1,16 @@
 function deepClone(obj){
-    let newObj = {},
-    toStr=Object.prototype.toString,
-    arrStr = '[object Array]';
+    if(typeof obj !=='object'){
+        new Object(obj)
+    }
+    let newObj = Array.isArray(obj)?[]:{},
+    toStr=Object.prototype.toString;
     for(let prop in obj){
         //排除原型
         if(obj.hasOwnProperty(prop)){
             //判断是否是对象还是原始值
             if(typeof(obj[prop])=='object'){
                 //判断是对象还是数组
-                toStr.call(obj[prop])==arrStr?[]:{};
+                toStr.call(Array.isArray(obj[prop]))?[]:{};
                 //递归拷贝
                 deepClone(obj[prop])
             }else{
